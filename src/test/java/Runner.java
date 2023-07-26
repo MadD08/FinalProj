e38e5ba98bc0e08dc6ab1f40a9b7299434ca439c
dmytro.ivanov <dmytro.ivanov@form.com>
2023-07-26 22:46:14 +0300

    1

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"C:/Users/Hedgehog/IdeaProjects/FinalProj/src/test/resources/features/BuyBook.feature"},
-        glue = {"src/test.java/steps"}
+        glue = {"src/test/java/steps"}
 )
 
 public class Runner {
